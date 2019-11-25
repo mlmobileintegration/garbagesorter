4b54781b9d08c260622280fd0ae5227b8a9f2f63
Muhammad Raza <ml.mobile.integration@gmail.com>
2019-11-25 10:33:32 -0500

    add object detectin api

--- app/server.py
+++ app/server.py
@@ -10,6 +10,15 @@ from pathlib import Path
 import uvicorn, aiohttp, asyncio
 import base64, sys, numpy as np
 
+import sys
+import cv2
+from datetime import datetime
+
+sys.path.append("..")
+from utils import ops as utils_ops
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
 path = Path(__file__).parent
 model_file_url = 'YOUR MODEL.h5 DIRECT / RAW DOWNLOAD URL HERE!'
 model_file_name = 'model'
@@ -20,6 +29,13 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 MODEL_PATH = path/'models'/f'{model_file_name}.h5'
 IMG_FILE_SRC = '/tmp/saved_image.png'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+BASE_PATH = path/'models'
+PATH_TO_FROZEN_GRAPH = BASE_PATH + '/graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = BASE_PATH + '/labelmap.pbtxt'
 
 async def download_file(url, dest):
     if dest.exists(): return
@@ -48,7 +64,7 @@ async def upload(request):
     img_bytes = await (data["img"].read())
     bytes = base64.b64decode(img_bytes)
     with open(IMG_FILE_SRC, 'wb') as f: f.write(bytes)
-    return model_predict(IMG_FILE_SRC, model)
+    return model_predict1(IMG_FILE_SRC)
 
 def model_predict(img_path, model):
     result = []; img = image.load_img(img_path, target_size=(224, 224))
@@ -60,10 +76,119 @@ def model_predict(img_path, model):
     result_html = str(result_html1.open().read() +str(result) + result_html2.open().read())
     return HTMLResponse(result_html)
 
+def model_predict1(img_path):
+    result = []; img = image.load_img(img_path)
+    # Actual detection.
+    output_dict = run_inference_for_single_image(img, graph)
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+        
+   
+    result_html1 = path/'static'/'result1.html'
+    result_html2 = path/'static'/'result2.html'
+    result_html = str(result_html1.open().read() +str(output_dict) + result_html2.open().read())
+    return HTMLResponse(result_html)
+
+
+def load_graph(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the 
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    # Then, we import the graph_def into a new Graph and returns it 
+    with tf.Graph().as_default() as graph:
+        # The name var will prefix every op/nodes in your graph
+        # Since we load everything in a new graph, this is not needed
+        tf.import_graph_def(graph_def, name="")
+    return graph
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+def run_inference_for_single_image(image, graph):
+    if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+    # Run inference
+    output_dict = sess.run(tensor_dict,
+                            feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+    output_dict['detection_classes'] = output_dict[
+        'detection_classes'][0].astype(np.uint8)
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+    if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+
+
 @app.route("/")
 def form(request):
     index_html = path/'static'/'index.html'
     return HTMLResponse(index_html.open().read())
 
 if __name__ == "__main__":
+
+    ##################################################
+    # Tensorflow part
+    ##################################################
+    print('Loading the model')
+    graph = load_graph(PATH_TO_FROZEN_GRAPH)
+
+    print('Loading the label map')
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+    print('Starting Session')
+    sess = tf.Session(graph=graph)
+
+    # Get handles to input and output tensors
+    tensor_dict = {}
+    with sess:
+        ops = tf.get_default_graph().get_operations()
+        all_tensor_names = {output.name for op in ops for output in op.outputs}
+
+        for key in [
+            'num_detections', 'detection_boxes', 'detection_scores',
+            'detection_classes', 'detection_masks'
+        ]:
+            tensor_name = key + ':0'
+            if tensor_name in all_tensor_names:
+                tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                tensor_name)
+
+    print(tensor_dict)
+
+    ##################################################
+    # END Tensorflow part
+    ##################################################
+
     if "serve" in sys.argv: uvicorn.run(app, host="0.0.0.0", port=8080)
