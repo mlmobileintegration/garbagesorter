43584e63af4ced69a5bd67e704ec2ed2eb74084d
Muhammad Raza <ml.mobile.integration@gmail.com>
2019-11-25 12:18:40 -0500

    Update

--- app/server.py
+++ app/server.py
@@ -109,16 +109,25 @@ def model_predict1(img_path):
 def load_graph(frozen_graph_filename):
     # We load the protobuf file from the disk and parse it to retrieve the 
     # unserialized graph_def
-    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
+    #with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+    #    graph_def = tf.GraphDef()
+    #    graph_def.ParseFromString(f.read())
 
     # Then, we import the graph_def into a new Graph and returns it 
-    with tf.Graph().as_default() as graph:
+    #with tf.Graph().as_default() as graph:
         # The name var will prefix every op/nodes in your graph
         # Since we load everything in a new graph, this is not needed
-        tf.import_graph_def(graph_def, name="")
-    return graph
+    #    tf.import_graph_def(graph_def, name="")
+    #return graph
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.gfile.GFile(frozen_graph_filename, 'rb') as fid:   
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    return detection_graph
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
